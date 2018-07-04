ef5d729ef66a22dd3c2683db157a8402b6c88df2
franshiro <franshiro@gmail.com>
2018-07-04 22:53:53 +0700

    tugas 7 looping dan asterix

---
+++ exercises7.js
@@ -0,0 +1,29 @@
+console.log('Exercises7 no.1');
+var rows1 = 5; // input the number of rows
+for(var i = 0; i < 5; i++){
+    console.log('*');
+}
+
+
+// do loops to display asterisks in the console.
+console.log('---------------------------------------');
+var rows2= 10; // input the number of rows
+for(var i = 0; i < rows2; i++){
+    asterix = '';
+    for(var j = 0; j < rows2; j++){
+        asterix += "*";
+    }
+    console.log(asterix);
+}
+
+// }
+// do loops to display asterisks in the console.
+console.log('---------------------------------------');
+var rows3 = 3;
+var bintang = ''
+for(var l = 0; l < rows3 ; l++){
+    bintang += "*"
+    console.log (bintang)
+}
+
+
