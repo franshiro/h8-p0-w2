fcd23d98a12b20cab4c907a89b134f37ec494064
franshiro <franshiro@gmail.com>
2018-07-19 02:45:06 +0700

    menghitung nilai xo

--- exercises13.js
+++ exercises13.js
@@ -1,14 +1,30 @@
 function xo(str){
-  var total = {}
-  str.split('').forEach(function(i){
-  total[i] = (total[i] || 0) + 1
-  })
-  if(total.x === total.o){
-    return true
+  // var total = {}
+  // str.split('').forEach(function(i){
+  // total[i] = (total[i] || 0) + 1
+  // })
+  // console.log(total)
+  // if(total.x === total.o){
+  //   return true
+  // }
+  // else{
+  //   return false
+  // }
+  var hitungX = 0
+  var hitungO = 0
+  for(var i=0; i<str.length; i++){
+    if(str[i] === 'x'){
+      // console.log('#')
+      hitungX++
+    }
+    else{
+      // console.log('*')
+      hitungO++
+    }
   }
-  else{
-    return false
-  }
+  // console.log(hitungO)
+  // console.log(hitungX)
+  return hitungO === hitungX
 }
 
 console.log(xo('xoxoxo')); // true
@@ -16,3 +32,4 @@ console.log(xo('oxooxo')); // false
 console.log(xo('oxo')); // false
 console.log(xo('xxxooo')); // true
 console.log(xo('xoxooxxo')); // true
+
